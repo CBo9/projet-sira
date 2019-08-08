82819c0644b7955626bb6576e164416db43c5791
Clement <clement.boilevin@gmail.com>
2019-08-08 14:40:16 +0200

    ajout du formulaire pour ajout de vehicules

--- ajoutv.php
+++ ajoutv.php
@@ -1,3 +1,9 @@
+<?php  
+
+require('fonctions.php')
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +13,7 @@
 </head>
 <body>
 	<h1>Ajoutez un véhicule</h1>
-	<form method="post" action="">
+	<form method="post" action="" enctype="multipart/form-data">
 		<fieldset>
 			<label>Ajoutez votre agence</label>
 			<input type="text" name="agence">
@@ -22,7 +28,7 @@
 			<input type="text" name="modele">
 			<br><br>
 			<label>Prix</label>
-			<input type="text" name="modele">
+			<input type="text" name="prixj">
 			<br><br>
 			<label>Décrivez le véhicule</label>
 			<input type="text" name="des">
@@ -30,7 +36,7 @@
 			<label>Ajoutez une photo</label>
 			<br><br>
 			<input type="file" name="mfichier">
-			<input type="image" name="img">
+		
 			<br><br>
 			<input type="submit" name="envoyer">
 		</fieldset>
@@ -38,7 +44,51 @@
 </body>
 </html>
 <?php
+if (isset($_POST['envoyer'])) {
+echo $_FILES['mfichier']['error'];
 
 
+if (isset($_FILES['mfichier']) AND $_FILES['mfichier']['error'] == 0)  {
 
+
+	if ($_FILES['mfichier']['size'] <= 1000000) {
+
+
+		// Extentions autorisées
+		$extension_autorisees = ["jpg", "jpeg", "png", "gif"];
+		$info= pathinfo($_FILES['mfichier']['name']);
+		
+		// Extentions de notre fichier
+		$extension_uploadee = $info['extension'];
+		
+		// On verifie l'extention
+		if (in_array($extension_uploadee, $extension_autorisees)) {
+			
+			move_uploaded_file($_FILES['mfichier']['tmp_name'], 'img/' .basename($_FILES['mfichier']['name']));
+			$image = basename($_FILES['mfichier']['name']);
+			$db=connexion('sira');
+			$insert=$db->prepare('INSERT INTO vehicule (titre, marque, modele, description, photo, prix_journalier) VALUES(:voiture, :marque, :modele, :des, :mfichier, :prixj)');
+							$insert->execute(['voiture'=>$_POST['voiture'],
+				  							  'marque' =>$_POST['marque'],
+				  							  'modele'=> $_POST['modele'],
+				  							  'des' => $_POST['des'],
+				  							'mfichier'=>$image,
+				  							'prixj'=>$_POST['prixj']]);
+			echo "Votre fichier a bien été envoyer";
+		}
+	
+	}
+
+	else{
+
+		echo "Votre fichier est trop volumineux!!! ";
+	}
+}
+
+
+                
+							
+
+}
 ?>
+
