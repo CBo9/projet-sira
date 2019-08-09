303d614ff64fb3b844c8f115b0288e702e4741bf
Clement <clement.boilevin@gmail.com>
2019-08-09 09:42:41 +0200

    rajout de la branche test de Clément

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 session_start();
 require('templates/navbar.php');?>
+
+<h1>Bienvenue sur Localoc</h1>
--- pages/ajoutv.php
+++ pages/ajoutv.php
@@ -1,6 +1,49 @@
-<?php  
+<?php
+require('../templates/navbar.php');
+require('../utility/fonctions.php');
+//INSERTION DES NOUVELLES DONNES DANS LA BDD APRES REMPLISSAGE DU FORM
 
-require('../utility/fonctions.php')
+if (isset($_POST['envoyer'])) {
+
+
+if (isset($_FILES['mfichier']) AND $_FILES['mfichier']['error'] == 0)  {
+
+
+	if ($_FILES['mfichier']['size'] <= 1000000) {
+
+
+		// Extentions autorisées
+		$extension_autorisees = ["jpg", "jpeg", "png", "gif"];
+		$info= pathinfo($_FILES['mfichier']['name']);
+		
+		// Extentions de notre fichier
+		$extension_uploadee = $info['extension'];
+		
+		// On verifie l'extention
+		if (in_array($extension_uploadee, $extension_autorisees)) {
+			
+			move_uploaded_file($_FILES['mfichier']['tmp_name'], '../img/' .basename($_FILES['mfichier']['name']));
+			$image = basename($_FILES['mfichier']['name']);
+			$db=connexion('sira');
+			$insert=$db->prepare('INSERT INTO vehicule (titre, marque, modele, description, photo, prix_journalier) VALUES(:voiture, :marque, :modele, :des, :mfichier, :prixj)');
+							$insert->execute(['voiture'=>$_POST['voiture'],
+				  							  'marque' =>$_POST['marque'],
+				  							  'modele'=> $_POST['modele'],
+				  							  'des' => $_POST['des'],
+				  							'mfichier'=>$image,
+				  							'prixj'=>$_POST['prixj']]);
+			
+		}
+	
+	}
+
+	else{
+
+		echo "Votre fichier est trop volumineux!!! ";
+	}
+}
+}
+
 
 ?>
 
@@ -11,7 +54,42 @@ require('../utility/fonctions.php')
 	<link rel="stylesheet" type="text/css" href="">
 	<title>Ajouter un véhicule</title>
 </head>
-<body>
+<body id="page_ajouta">
+
+
+<!--  DEBUT DE L'AFFICHAGE DU TABLEAU DES VEHICULES DE LA BDD-->	
+	<h1>Nos véhicules</h1>
+	<table>
+		<tr>
+			<td>Numéro de série</td>
+			<td>Photo</td>
+			<td>Marque</td>
+			<td>Modèle</td>
+			<td>Description</td>
+			<td>Prix (à la journée)</td>
+			<td>Situé à l'agence</td>
+		</tr>
+<?php 
+//DEBUT DE LA REQUETE 
+$connect=connexion('sira');
+$requete=$connect->prepare('SELECT * FROM vehicule AS v ');
+$requete->execute();
+while($donnees =$requete->fetch()){
+	echo "<tr>
+			<td> ". $donnees['id_vehicule'] . "</td>
+			<td><img src='../img/" . $donnees['photo'] . "' class='photoTab'></td>
+			<td>" . $donnees['marque'] ."</td>
+			<td>" . $donnees['modele'] ."</td>
+			<td>". $donnees['description']."</td>
+			<td>". $donnees['prix_journalier']." </td>
+			<td>".$donnees['id_agence']." </td>
+		</tr>";
+}
+?>
+<table>
+<!--  FIN DE L'AFFICHAGE DU TABLEAU DES VEHICULES DE LA BDD-->			
+
+<!-- DEBUT DE L'AJOUT DE VEHICULE DANS LA BSS-->
 	<h1>Ajoutez un véhicule</h1>
 	<form method="post" action="" enctype="multipart/form-data">
 		<fieldset>
@@ -43,52 +121,4 @@ require('../utility/fonctions.php')
 	</form>
 </body>
 </html>
-<?php
-if (isset($_POST['envoyer'])) {
-echo $_FILES['mfichier']['error'];
-
-
-if (isset($_FILES['mfichier']) AND $_FILES['mfichier']['error'] == 0)  {
-
-
-	if ($_FILES['mfichier']['size'] <= 1000000) {
-
-
-		// Extentions autorisées
-		$extension_autorisees = ["jpg", "jpeg", "png", "gif"];
-		$info= pathinfo($_FILES['mfichier']['name']);
-		
-		// Extentions de notre fichier
-		$extension_uploadee = $info['extension'];
-		
-		// On verifie l'extention
-		if (in_array($extension_uploadee, $extension_autorisees)) {
-			
-			move_uploaded_file($_FILES['mfichier']['tmp_name'], '../img/' .basename($_FILES['mfichier']['name']));
-			$image = basename($_FILES['mfichier']['name']);
-			$db=connexion('sira');
-			$insert=$db->prepare('INSERT INTO vehicule (titre, marque, modele, description, photo, prix_journalier) VALUES(:voiture, :marque, :modele, :des, :mfichier, :prixj)');
-							$insert->execute(['voiture'=>$_POST['voiture'],
-				  							  'marque' =>$_POST['marque'],
-				  							  'modele'=> $_POST['modele'],
-				  							  'des' => $_POST['des'],
-				  							'mfichier'=>$image,
-				  							'prixj'=>$_POST['prixj']]);
-			echo "Votre fichier a bien été envoyer";
-		}
-	
-	}
-
-	else{
-
-		echo "Votre fichier est trop volumineux!!! ";
-	}
-}
-
-
-                
-							
-
-}
-?>
 
